0f7c5a06ab6e0ef33db22cee63a912cbd1661bd0
Aluisio Carvalho <aluisio.carvalho@iesb.edu.br>
2024-11-04 10:01:40 -0300

    aula recursividade;

---
+++ aulas/aula7.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int *criar_vetor(int n_elementos, int inicializaçao) {
+    int *vet = malloc(sizeof(int) * n_elementos);
+    if (vet != NULL) {
+
+
+    } 
+
+    return vet;
+}
+void exibir_vetor(int n_elementos, int *vet) {
+    if(n_elementos < 0 ) return;
+    for(int i = 0; i < n_elementos; i++) {
+        printf
+
+
+    }
+
+
+}
+
+int main() {
+    int *dynamic_num;
+
+
+
+   return 0;
+} 
+
+
+
